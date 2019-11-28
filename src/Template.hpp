28f9d0b02d071e09acc155d282fee1545d5c76eb
Bo Cao <caobo.github@gmail.com>
2019-11-28 12:39:59 -0500

    Template.hpp created.

---
+++ src/Template.hpp
@@ -0,0 +1,21 @@
+/*
+ * Introduction to the header template.
+ * For each function FUNC, the file name shall be FUNC.hpp
+ * Every FUNC shall be replaced with implemented function name.
+ * The type of a CFS with FLOAT is CFST<FLOAT>
+ * The code shall follow c++17 standard.
+ * Only the c++ standard libraries are allowed.
+ * C-style code is highly not recommended.
+ */
+#ifndef FSL_FUNC_H
+#define FSL_FUNC_H
+
+/*Library inclusion is here*/
+
+template<class FLOAT, template<class> class CFST>
+/*RETURN TYPE*/ FUNC(/*PARAMETER LIST*/)
+{
+    /*FUNCTION BODY*/
+}
+
+#endif
